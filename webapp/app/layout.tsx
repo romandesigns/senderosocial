08e3d68e5b13111047782e5d6d64fbf1398a31b7
Roman F. <wavystyledev@gmail.com>
2025-02-27 21:30:58 -0500

    Installed shadcn theme

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
+import {ModeToggle} from "@/components/ThemeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
       >
+<ModeToggle/>
+
         {children}
+      </ThemeProvider>
       </body>
     </html>
   );
